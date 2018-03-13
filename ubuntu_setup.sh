2e37206fdc78201cbd68aea51a538e3cb3f14fc8
pkspats <faith.prashant@gmail.com>
2018-03-14 00:02:17 +0530

    ubuntu initial setup and python initial setup files

---
+++ ubuntu_setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo "downloading and installing chromium browser..."
+sudo apt install chromium-browser
+
+echo "downloading and installing git..."
+sudo apt install git
+
+echo "install pip..."
+sudo apt install python-pip
+
+echo "Cloning git folders..."
+cd /
+sudo git clone https://github.com/pkspats/Neural-Network-Projects.git
+sudo git clone https://github.com/pkspats/Automation_Scripts.git
+
+echo "Installing Themes...."
+sudo apt remove unity-tweak-tool && sudo apt install -y gnome-tweak-tool
+sudo apt install -y numix-gtk-theme
+sudo apt install -y numix-icon-theme numix-icon-theme-circle
+sudo apt install -y pop-theme
+sudo apt install -y adapta-gtk-theme
+sudo apt install -y vertex-theme
+sudo apt install -y arc-theme
